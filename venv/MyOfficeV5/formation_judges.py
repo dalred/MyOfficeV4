441f4292bfd8d88af18fce675db75daba3d0f5d1
dalred <RcrF63gGG7HpbAT>
2020-08-17 04:38:30 +0300

    Добавление с номерами без разрывов

--- venv/MyOfficeV5/formation_judges.py
+++ venv/MyOfficeV5/formation_judges.py
@@ -3,23 +3,23 @@ from MyOfficeSDKDocumentAPI import DocumentAPI as sdk
 import inspect, os
 
 
-def write_formules(n_rows,fio_list,table_output2_xlsx,table_output_xlsx,document2_xls,mydirs_,worker):
+def write_formules(row_str,fio_list,table_output2_xlsx,table_output_xlsx,mydirs_,worker):
     worker.ReportProgress(10, u"Формирование файлов Жюри")
     current_row = 4 # c index 4
-    for i in range(current_row, n_rows + 1):
-        index = i - 3
-        n_rows = str(i)
+    for str_ in fio_list:
+        index = current_row - 3
         percentage = (index * 100) / len(fio_list)
         worker.ReportProgress(percentage, u"Формирование файлов Жюри")
-        for str_ in fio_list:
-            table_output2_xlsx.getCell("B" + n_rows).setText(str_)
-        table_output2_xlsx.getCell("A" + n_rows).setNumber(index)
-        table_output2_xlsx.getCell("F" + n_rows).setFormula("=SUM(C" + n_rows + ":D" + n_rows + ":E" + n_rows + ")")
-        table_output2_xlsx.getCell("K" + n_rows).setFormula("=SUM(G" + n_rows + ":H" + n_rows + ":I" + n_rows + ":J" + n_rows + ")")
-        table_output_xlsx.getCell("W" + n_rows).setFormula("=AVERAGE(AC" + n_rows + ":AE" + n_rows + ")+SUM(" + "X" + n_rows + ":AB" + n_rows + ")")
-        table_output_xlsx.getCell("AF" + n_rows).setFormula("=AVERAGE(AG" + n_rows + ":AI" + n_rows + ")")
-        table_output_xlsx.getCell("AJ" + n_rows).setFormula("=SUM(AF" + n_rows + ",W" + n_rows + ")")
-        document2_xls.saveAs(mydirs_[12].encode('utf-8'))
+        row_str = str(current_row)
+        table_output2_xlsx.getCell("B" + row_str).setText(str_)
+        table_output2_xlsx.getCell("A" + row_str).setNumber(index)
+        table_output2_xlsx.getCell("F" + row_str).setFormula("=SUM(C" + row_str + ":D" + row_str + ":E" + row_str + ")")
+        table_output2_xlsx.getCell("K" + row_str).setFormula("=SUM(G" + row_str + ":H" + row_str + ":I" + row_str + ":J" + row_str + ")")
+        table_output_xlsx.getCell("W" + row_str).setFormula("=AVERAGE(AC" + row_str + ":AE" + row_str + ")+SUM(" + "X" + row_str + ":AB" + row_str + ")")
+        table_output_xlsx.getCell("AF" + row_str).setFormula("=AVERAGE(AG" + row_str + ":AI" + row_str + ")")
+        table_output_xlsx.getCell("AJ" + row_str).setFormula("=SUM(AF" + row_str + ",W" + row_str + ")")
+        current_row += 1
+
 
 def main_judges(worker,mydirs_):
     worker.ReportProgress(0, u"Формирование файлов Жюри")
@@ -32,11 +32,11 @@ def main_judges(worker,mydirs_):
     document2_xls = application.loadDocument(input_file_url2_xls)  # load Жюри1
     table_output2_xlsx = document2_xls.getBlocks().getTable(0)  # table Жюри1
 
-    n_rows = table_output_xlsx.getRowsCount()  # количество строк
+    row_str = table_output_xlsx.getRowsCount()  # количество строк
     E4_empty = table_output_xlsx.getCell("E4").getRawValue() == ''
 
     if not E4_empty:
-        cell_range = table_output_xlsx.getCellRange("E4:E" + str(n_rows))
+        cell_range = table_output_xlsx.getCellRange("E4:E" + str(row_str))
         fio_list = [i.getRawValue() for i in cell_range]
 
     # Выделение строк в таблице по количеству строк из документов
@@ -47,7 +47,9 @@ def main_judges(worker,mydirs_):
     elif rows_count_2 == 1:
         document2_xls.saveAs(mydirs_[12].encode('utf-8'))
 
-    write_formules(n_rows,fio_list,table_output2_xlsx,table_output_xlsx,document2_xls,mydirs_,worker)
+    write_formules(row_str,fio_list,table_output2_xlsx,table_output_xlsx,mydirs_,worker)
+    document_xls.saveAs(mydirs_[11].encode('utf-8'))
+    document2_xls.saveAs(mydirs_[12].encode('utf-8'))
     table_output2_xlsx.getCell("B1").setText("Жюри 2")
     document2_xls.saveAs(mydirs_[13].encode('utf-8'))
     table_output2_xlsx.getCell("B1").setText("Жюри 3")
